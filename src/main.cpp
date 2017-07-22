d178ade04cfeb27f8efcdb0c012c5eff20f9c2d1
Rodrigo Caetano Rocha <rcor.cs@gmail.com>
2017-07-22 22:40:40 +0100

    updates main

--- src/main.cpp
+++ src/main.cpp
@@ -23,51 +23,49 @@ int yylex();
 void yyparse();
 int newfile(char *fn);
 
-void testlex(char *file);
-void testparse(char *file);
+void scan(char *file);
 
-int main(int argc, char **argv)
-{
-   //printctypes();
-   //printrcctypes();
-   //if(argc>1) testlex(argv[1]);
-   if(argc>1) testparse(argv[1]);
+void compile(char *file);
 
+int main(int argc, char **argv){
+   for(int fileid = 1; fileid<argc; fileid++){
+      compile(argv[fileid]);
+   }
    return 0;
 }
 
-void testparse(char *file)
-{
+void compile(char *file){
    if(!newfile(file)) return;
-   //symbolTable.pushScope();
+
    yyparse();
 
    ASTVisitor visitor;
    visitor.visit(astRoot);
 
+   /*
    cout << "Pretty Printer: CGen" << endl;
    ASTCGen cgen;
    cout << cgen.visit(astRoot) << endl;
+   */
 
    TypeChecker typecheck;
    typecheck.visit(astRoot);
-   //delete symbolTable.popScope();
 
    ASTLLGen llgen(file);
    llgen.visit(astRoot);
+
    cout << "Dumping Module" << endl;
    llgen.dump();
-   string filename(file);
-   llgen.writeAssembly(filename+".ll");
+
+   //string filename(file);
+   //llgen.writeAssembly(filename+".ll");
 }
 
-void testlex(char *file)
-{
+void scan(char *file){
    if(!newfile(file)) return;
    int token;
    do{
      token = yylex();
    }while(token!=0);
 }
-
 
